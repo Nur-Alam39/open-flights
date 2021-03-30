bd60277f08e864223f80565ed9838a3216da49d8
Nur-Alam39 <nuralam.cse.bsmrstu@gmail.com>
2021-03-30 16:12:18 +0600

    added json api serailizer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+                            {
+                              name: "Bangladesh Airlines",
+                              image_url: "https://thefinancialexpress.com.bd/uploads/1511600741.jpg"
+                            },
+                            {
+                              name: "Southwest",
+                              image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+                            },
+                            {
+                              name: "Delta",
+                              image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+                            },
+                            {
+                              name: "Alaska Airlines",
+                              image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+                            },
+                            {
+                              name: "JetBlue",
+                              image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+                            },
+                            {
+                              name: "American Airlines",
+                              image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+                            }
+                          ])
+
+reviews = Review.create([
+                          {
+                            title: "Great Airline",
+                            description: "I had a good trip",
+                            score: 4.5,
+                            airline:airlines.first
+                          },
+                          {
+                            title: "Not a bad Airline",
+                            description: "I had a average trip experience",
+                            score: 3.5,
+                            airline:airlines.first
+                          },
+                          {
+                            title: "Great Airline",
+                            description: "I had a good trip",
+                            score: 4.5,
+                            airline:airlines.second
+                          },
+                          {
+                            title: "Not a bad Airline",
+                            description: "I had a average trip experience",
+                            score: 3.5,
+                            airline:airlines.second
+                          },
+
+                        ])
